d9c83d7cd7a79ef46ff264880788685a84504a02
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-01-30 01:51:36 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-full bg-secondary py-6 px-5'>
+        <p className='text-gray-400 text-xs opacity-80'>© 2023 Copyright <span className='font-bold'>FSW Barber</span></p>
+    </footer>
+  )
+}
+
+export default Footer
