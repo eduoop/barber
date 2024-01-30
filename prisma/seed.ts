100d8850ae29f4c978d10718d972a34e38382c4e
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-01-30 20:36:11 -0300

    fix: service image url

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -125,7 +125,7 @@ async function seedDatabase() {
                 id: barbershop.id,
               },
             },
-            imageUrl: service.imageUrl
+            imageUrl: service.imageUrl,
           },
         });
       }
