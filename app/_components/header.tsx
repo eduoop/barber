e6b1a9c64599ebe5fd31d788656af972a6149fc7
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-01-30 23:38:35 -0300

    feat: add google authentication

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,10 +1,20 @@
+"use client"
+
 import React from "react";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import {signIn, useSession} from "next-auth/react"
 
 const Header = () => {
+
+  const { data } = useSession();
+
+  const handleLogin = async () => {
+    await signIn();
+  }
+
   return (
     <Card>
       <CardContent className="p-5 justify-between flex flex-row items-center">
